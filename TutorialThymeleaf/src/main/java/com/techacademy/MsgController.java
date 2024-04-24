1512257116483aac39700751e4bf3bd59ab13b5f
dir-FY24furutamanami <manami.furuta.r738@dirbato.co.jp>
2024-04-24 17:41:07 +0900

    Lesson20

---
+++ TutorialThymeleaf/src/main/java/com/techacademy/MsgController.java
@@ -0,0 +1,17 @@
+package com.techacademy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class MsgController {
+
+    @GetMapping("/msg")
+    public String getMsg() {
+        // msg.htmlに画面遷移
+        return "msg";
+    }
+    
+}
